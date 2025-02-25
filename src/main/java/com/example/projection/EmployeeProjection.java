e9f6be637fb14ce9abb3f5db8d8c4b18469c1653
Aayu Parikh <aryanparikh03@gmail.com>
2025-02-25 16:06:38 +0530

    Open projection implemented

--- src/main/java/com/example/projection/EmployeeProjection.java
+++ src/main/java/com/example/projection/EmployeeProjection.java
@@ -1,7 +1,12 @@
 package com.example.projection;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface EmployeeProjection {
     String getFirstName();
     String getEmail();
     Double getSalary();
+
+    @Value("#{target.firstName + ' ' + target.lastName}")
+    String getFullName();
 }
